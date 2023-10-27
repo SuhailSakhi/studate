a18254dd618775bc17402d1a9b09a42187717438
SuhailSakhi <suhail_909@outlook.com>
2023-10-27 19:24:10 +0200

    foutmeldingen toegevoegd code is compleet

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -17,20 +17,39 @@ class CreateController extends Controller
 
     public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
-        $user = Auth::user();
         $validator = Validator::make($request->all(), [
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
             'name' => 'required|string|max:255',
             'bio' => 'required|string',
             'gender' => 'required|in:male,female,other',
             'age' => 'required|integer|min:18|max:30',
+        ], [
+            'image.required' => 'De afbeelding is verplicht.',
+            'image.image' => 'De afbeelding moet een geldig afbeeldingsbestand zijn (jpeg, png, jpg, gif).',
+            'image.mimes' => 'De afbeelding moet van het type jpeg, png, jpg of gif zijn.',
+            'image.max' => 'De afbeelding mag niet groter zijn dan 2 MB.',
+            'name.required' => 'De naam is verplicht.',
+            'name.max' => 'De naam mag niet langer zijn dan 255 tekens.',
+            'bio.required' => 'De bio is verplicht.',
+            'gender.required' => 'Het geslacht is verplicht.',
+            'gender.in' => 'Het geslacht moet man, vrouw of anders zijn.',
+            'age.required' => 'De leeftijd is verplicht.',
+            'age.integer' => 'De leeftijd moet een geheel getal zijn.',
+            'age.min' => 'De leeftijd moet minimaal 18 jaar zijn.',
+            'age.max' => 'De leeftijd mag niet ouder zijn dan 30 jaar.',
         ]);
 
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator)
-                ->withInput();
-        }
+
+//        if ($validator->fails()) {
+//            return redirect()->back()
+//                ->withErrors($validator)
+//                ->withInput();
+            if ($validator->fails()) {
+                return redirect()->route('create')
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+
 
         $imagePath = $request->file('image')->store('/images', 'public');
         $imagePath = str_replace('images/', '', $imagePath);
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,62 +2,64 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>profiel aanmaken</title>
+    <title>Profiel aanmaken</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
-<form method="POST" action="{{ route('create') }}" enctype="multipart/form-data">
-    @csrf
+<div class="container">
+    <form method="POST" action="{{ route('create') }}" enctype="multipart/form-data">
+        @csrf
 
-    <div class="mb-3">
-        <label for="image" class="form-label">Profielfoto</label>
-        <input type="file" class="form-control" id="image" name="image" required>
-        @error('image')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Profielfoto</label>
+            <input type="file" class="form-control" id="image" name="image" required>
+            @error('image')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-        <label for "name" class="form-label">Naam</label>
-        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required autofocus>
-        @error('name')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="name" class "form-label">Naam</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required autofocus>
+            @error('name')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-        <label for="gender" class="form-label">Geslacht</label>
-        <select class="form-select" id="gender" name="gender" required>
-            <option value="male">Man</option>
-            <option value="female">Vrouw</option>
-            <option value="other">Anders</option>
-        </select>
-        @error('gender')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="gender" class="form-label">Geslacht</label>
+            <select class="form-select" id="gender" name="gender" required>
+                <option value="male">Man</option>
+                <option value="female">Vrouw</option>
+                <option value="other">Anders</option>
+            </select>
+            @error('gender')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-        <label for="age" class="form-label">Leeftijd</label>
-        <input type="text" class="form-control" id="age" name="age" value="{{ old('age') }}" required>
-        @error('age')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="age" class="form-label">Leeftijd</label>
+            <input type="text" class="form-control" id="age" name="age" value="{{ old('age') }}" required>
+            @error('age')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-        <label for="bio" class="form-label">Bio</label>
-        <textarea class="form-control" id="bio" name="bio" required>{{ old('bio') }}</textarea>
-        @error('bio')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="bio" class="form-label">Bio</label>
+            <textarea class="form-control" id="bio" name="bio" required>{{ old('bio') }}</textarea>
+            @error('bio')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="mb-3">
-        <button type="submit" class="btn btn-primary">Profiel aanmaken</button>
-    </div>
-</form>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Profiel aanmaken</button>
+        </div>
+    </form>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
@@ -65,3 +67,4 @@
 
 </body>
 </html>
+
